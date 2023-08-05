be49f894dc32bff36731b0b54e05f62419b576ab
JNart8 <narteyj8@gmail.com>
2023-08-05 03:28:45 +0000

    personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,36 +1,16 @@
 #!/usr/bin/env python3
+"""A module for encrypting passwords.
 """
-   User passwords should NEVER be stored in plain
-   text in a database
-"""
-
-
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    ''' Implement a hash_password function that expects one string
-        argument name password and returns a salted, hashed
-        password, which is a byte string.
-        Use the bcrypt package to perform the hashing (with hashpw).
-    '''
-    encoded = password.encode()
-    hashed_val = bcrypt.hashpw(encoded, bcrypt.gensalt())
-
-    return hashed_val
+    """Hashes a password using a random salt.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    '''
-       is_valid function expects 2 arguments and
-        returns a boolean.
-       Arguments:hashed_password: bytes type
-                password: string type
-       Use bcrypt to validate that the provided password matches the hashed
-       password.
-    '''
-    valid = False
-    pass_encoded = password.encode()
-    if bcrypt.checkpw(pass_encoded, hashed_password):
-        valid = True
-    return valid
+    """Checks is a hashed password was formed from the given password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
